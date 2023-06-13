0e2a3056366f784c75ae67bdf5393771723979eb
anto <antobenis100@gmail.com>
2023-06-13 12:02:05 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import { Routes, Route,Navigate } from 'react-router-dom'
 import './App.css';
-
+import { lazy } from 'react';
+import { Suspense } from 'react';
+const Login = lazy(() => import('./login/Login.jsx'))
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Suspense>
+        <Routes>
+          <Route path="/" element={<Navigate to={'/login'} />} replace />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Suspense>
+    </>
   );
 }
 
